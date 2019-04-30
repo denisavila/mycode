ec64fe1cccb21282de0859276eb2645c9171a657
denisavila <denisavila@gmail.com>
2019-04-30 10:33:49 -0400

    New commit

---
+++ condition02.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+hostname = input("What should we set the hostname?")
+if hostname == 'MTG':print('The hostname was found to be mtg')
---
+++ condition03.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+hostname = input("What should we set the hostname?")
+if hostname.lower() == 'mtg':
+	print('The hostname was found to be mtg')
+	print("Matches config")
+print("Exit prgm")
