c353c58e0dc39dd724747936a06fadb15fa79dcc
denisavila <denisavila@gmail.com>
2019-05-03 13:34:43 -0400

    Your comment here!

--- catfacts/catfact1_.py
+++ catfacts/catfact1_.py
@@ -7,7 +7,7 @@ import requests
 def main():
     """Run time code"""
     # create r, which is our request object
-    r = requests.get("https://cat-fact.herokuapp.com/facts")
+    r = requests.get("https://cat-fact.herokuapp.com")
 
     # display the methods available to our new object
     print( dir(r) )
